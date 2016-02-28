4f616e8241e6d38152e7d6dda139206a0bae828c
Dario Aguilera <dario_21_06@hotmail.com>
2016-02-28 02:30:49 -0300

    Dialer and contacts app are now not using bold letters.

--- app/src/main/java/com/samkpo/dario/noboldcontacts/NoBold.java
+++ app/src/main/java/com/samkpo/dario/noboldcontacts/NoBold.java
@@ -1,41 +1,52 @@
 package com.samkpo.dario.noboldcontacts;
 
+import android.content.res.XModuleResources;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
-import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 /**
  * Created by dario on 27/02/16.
  */
-public class NoBold implements IXposedHookLoadPackage{//},IXposedHookInitPackageResources {
+public class NoBold implements IXposedHookZygoteInit,IXposedHookInitPackageResources {
     private static String MODULE_PATH = null;
-    private static String APP_TO_HACK = "com.android.dialer";
+    private static String APPS_TO_HACK[] = {
+            "com.android.dialer",
+            "com.android.contacts"
+    };
 
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals(APP_TO_HACK))
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        MODULE_PATH = startupParam.modulePath;
+    }
+
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        if (!lpparam.packageName.equals(APPS_TO_HACK[0]) && !lpparam.packageName.equals(APPS_TO_HACK[1]))
             return;
 
-        XposedBridge.log("We are in AOSP Dialer");
+        XposedBridge.log("\n\n\n\nWe are in AOSP Dialer\n\n\n\n");
 
         XposedBridge.log("Version: " + (BuildConfig.DEBUG ? BuildConfig.VERSION_NAME + " - " + BuildConfig.VERSION_CODE
                 : BuildConfig.VERSION_NAME));
     }
 
-    /*@Override
+    @Override
     public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
-        if (!resparam.packageName.equals(APP_TO_HACK))
+        if (!resparam.packageName.equals(APPS_TO_HACK[0]) && !resparam.packageName.equals(APPS_TO_HACK[1]))
             return;
 
         //Get resources
         //XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
 
         //Primary colors
-        //String[] _primaryColors = modRes.getStringArray(R.array.primary_colors_to_replace);
-        //for(String pc : _primaryColors)
-        //    resparam.res.setReplacement(APP_TO_HACK, "color", pc, modRes.getColor(R.color.colorPrimary));
+        resparam.res.setReplacement(resparam.packageName, "string", "letter_tile_letter_font_family", "sans-serif-light");
 
-    }*/
+        XposedBridge.log("Replacing data in contacts.");
+
+    }
+
 }
